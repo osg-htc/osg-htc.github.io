b1b6f4c58769571888c07acb923eded685959646
Cannon Lock <clock@wisc.edu>
2025-03-04 17:12:13 -0600

    Update OSG Homepage

--- assets/js/all.js
+++ assets/js/all.js
@@ -12,3 +12,47 @@ document.querySelectorAll('.dropdown-menu a').forEach(function(element) {
 		});
 	}
 })
+
+/**
+ * Finds all the elements with '.fade-in' and fades them in as they are scrolled onto the screen.
+ */
+const fadeInElements = document.querySelectorAll('.fade-in');
+
+// Add a fade-in-active class so we know js is running
+fadeInElements.forEach(element => {
+	element.classList.add('fade-in-active');
+});
+
+// Run the following function when the element is scrolled into view
+const fadeInOptions = {
+	threshold: 1,
+	rootMargin: "0px 0px 0px 0px"
+};
+const fadeInObserver = new IntersectionObserver((entries, observer) => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('fade-in-visible');
+		}
+	});
+}, fadeInOptions);
+fadeInElements.forEach(element => {
+	fadeInObserver.observe(element);
+});
+
+/** Add a function that checks if the element #reverse-fade-in is hovered and if so reverse it */
+const touchScreen = matchMedia('(hover: none)').matches;
+const reverseFadeInElement = document.querySelector('#reverse-fade-in');
+if (reverseFadeInElement && !touchScreen) {
+	reverseFadeInElement.addEventListener('mouseover', () => {
+		fadeInElements.forEach(element => {
+			element.classList.remove('fade-in-visible');
+		});
+	});
+	reverseFadeInElement.addEventListener('mouseout', () => {
+		fadeInElements.forEach(element => {
+			if (element.classList.contains('fade-in-active')) {
+				element.classList.add('fade-in-visible');
+			}
+		});
+	});
+}
